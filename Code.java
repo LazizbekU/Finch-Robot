25213dba80d427128119a923bf75dba65226ce15
LazizbekU <lazizbeku@nycstudents.net>
2025-06-03 11:31:29 -0400

    Create Code.java

---
+++ Code.java
@@ -0,0 +1,18 @@
+public class FinchTest {
+    public static void main(String[] args) {
+        Finch myFinch = new Finch();
+        while(myFinch.getDistance()>20) {
+        	myFinch.setMove("F", 10, 50);
+        }
+        myFinch.stop(5);
+        int turn = (Math.random*180);
+        if(turn>90) {
+        	myFinch.setTurn("L", turn, 50);
+        
+        }else {
+        	myFinch.setTurn("R", turn+90, 50);
+        }
+        //myFinch.stopAll();
+        //myFinch.disconnect();
+    }
+}
