cb33864292ec5273a05fee501b4fa7d1b8ca698f
LazizbekU <lazizbeku@nycstudents.net>
2025-06-11 19:39:31 -0400

    Update Code.java

--- Code.java
+++ Code.java
@@ -1,19 +1,23 @@
 public class FinchTest {
     public static void main(String[] args) {
-        for(int i = 0; i<30; i++){
-            Finch myFinch = new Finch();
-            while(myFinch.getDistance()>20) {
-        	    myFinch.setMove("F", 10, 50);
+        Finch myFinch = new Finch(); // Only create once!
+
+        for (int i = 0; i < 30; i++) {
+            while (myFinch.getDistance() > 20) {
+                myFinch.setMove("F", 10, 50);
             }
-            myFinch.stop(30);
-            int turn = Math.random()*180;
-            if(turn>90) {
-        	    myFinch.setTurn("L", turn, 50);
-        
-            }else {
-        	    myFinch.setTurn("R", turn+90, 50);
+
+            myFinch.stop();
+            myFinch.sleep(30000); // Wait 30 seconds
+
+            int turn = (int)(Math.random() * 180); // 0–179
+            if (turn > 90) {
+                myFinch.setTurn("L", turn, 50);
+            } else {
+                myFinch.setTurn("R", turn + 90, 50);
             }
         }
+
         myFinch.stop();
         myFinch.quit();
     }
